ac3c58634063891d710de6d79229e4182a44bd88
Yasunari Ishibashi <izibazi@gmail.com>
2024-07-12 08:29:15 +0900

    テストケースを修正

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
-	if (result != "even") {
+	if (result != "evexn") {
 		t.Errorf("EvenOrOdd(10) = %s; want even", result)
 	}
 }
